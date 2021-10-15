1885e9c24a975bd87f4d06768fb3fecd8bacaf57
Ahmed Aziz ABBASSI <77698923+ahmed-99882@users.noreply.github.com>
2021-10-15 11:30:37 +0100

    Create sum_of_2_numbers.cpp

---
+++ sum_of_2_numbers.cpp
@@ -0,0 +1,16 @@
+// Sum of two numbers
+// Solution proposed by ABBASSI Ahmed Aziz
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+    cout << "Enter an integer a = " << endl;
+    cin >> a;
+    cout << "Enter an integer b = " << endl;
+    cin >> b;
+    sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is " << sum << endl;
+    return 0;
+}
